55a6bebd8733bd03031c8d6d22cd82f6ef469f54
An Tae Gyun <primarchan@gmail.com>
2023-03-17 14:01:51 +0900

    first commit

---
+++ src/main/java/com/example/springbootmongodbchattingserver/SpringbootMongodbChattingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmongodbchattingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbChattingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMongodbChattingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootmongodbchattingserver/SpringbootMongodbChattingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmongodbchattingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbChattingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
